cdb9e74e510f177f8618591243f4716233557e22
AhmedRabea0302 <arabea169@gmail.com>
2024-12-15 16:52:53 +0200

    Added get user feed endpoint

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -4,4 +4,16 @@ import "net/http"
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
 
+	ctx := r.Context()
+
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(99))
+	if err != nil {
+		app.intetrnalServerError(w, r, err)
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, feed); err != nil {
+		app.intetrnalServerError(w, r, err)
+		return
+	}
 }
